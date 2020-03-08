1d0a2865c47e4d5bca2074b3e0ec31b4b3cfe3a4
Peak Xin <xinyflove@sina.com>
2020-03-08 11:04:10 +0800

    Webpack对html模版的处理

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,22 @@
  * @Author: Peak Xin 
  * @Date: 2020-03-07 21:11:27 
  * @Last Modified by: Peak Xin
- * @Last Modified time: 2020-03-07 22:26:48
+ * @Last Modified time: 2020-03-08 10:49:59
  */
-var webpack           = require('webpack');
+var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+// 获取html-webpack-plugin参数的方法
+var getHtmlConfig = function (name) {
+    return {
+        template: './src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        inject: true,
+        hash: true,
+        chunks: ['common', name]
+    };
+}
+// webpack config
 var config = {
     entry: {
         'common': ['./src/page/common/index.js'],// 通用模块js
@@ -22,16 +33,20 @@ var config = {
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")}
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
         ]
     },
     plugins: [
-        // 处理js的通用模块
+        // 独立通用模块到js/base.js
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             filename: 'js/base.js'
         }),
-        new ExtractTextPlugin('css/[name].css')
+        // 把css单独打包到文件里
+        new ExtractTextPlugin('css/[name].css'),
+        // html模版的处理
+        new HtmlWebpackPlugin(getHtmlConfig('index')),
+        new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
 };
 
