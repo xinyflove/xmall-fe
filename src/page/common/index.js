1aa53638917baae787247960edab75eeba34c10c
Peak Xin <xinyflove@sina.com>
2020-03-10 22:43:50 +0800

    m

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,8 +2,8 @@
  * @Author: Peak Xin 
  * @Date: 2020-03-07 21:53:45 
  * @Last Modified by: Peak Xin
- * @Last Modified time: 2020-03-07 21:56:36
+ * @Last Modified time: 2020-03-10 22:43:21
  */
 
  'use strict';
- console.log('i am global');
+ require('./layout.css');
